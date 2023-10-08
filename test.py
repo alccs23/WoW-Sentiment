197eda1c949404b322b079a2b0faa2787acbf948
Fernando <fernandobadia16@gmail.com>
2023-10-07 22:45:22 -0400

    im stuff

---
+++ test.py
@@ -0,0 +1,22 @@
+import requests
+import json
+# Define the URL
+url = "https://us.forums.blizzard.com/en/wow/c/community/general-discussion/171/l/latest.json?ascending=false&no_definitions=true&page=1"
+
+# Send an HTTP GET request
+response = requests.get(url)
+
+# Check if the request was successful (status code 200)
+if response.status_code == 200:
+    # Parse the JSON response
+    data = response.json() 
+    # Access and process the data as needed
+    # For example, you can print the JSON data
+    topics = data['topic_list']
+
+    slug = topics['topics']
+    actuallySlug = slug[0]['slug']
+    print(actuallySlug)
+    if
+else:
+    print(f"Failed to retrieve data. Status code: {response.status_code}")
