d1192cb6d453896d2103858562f7bcc3b487d5fb
Fernando <fernandobadia16@gmail.com>
2023-10-07 22:21:07 -0400

    Playing around with data extraction

--- PlaygroundToPLay.py
+++ PlaygroundToPLay.py
@@ -1,5 +1,5 @@
 import requests
-
+import json
 # Define the URL
 url = "https://us.forums.blizzard.com/en/wow/c/community/general-discussion/171/l/latest.json?ascending=false&no_definitions=true&page=1"
 
@@ -11,8 +11,33 @@ if response.status_code == 200:
     # Parse the JSON response
     data = response.json()
     
+     # Specify the output file name
+    output_file = "forum_data.json"
+    
+    # Write the JSON data to the output file
+    with open(output_file, 'w') as json_file:
+        json.dump(data, json_file, indent=4)
+    
+    print(f"Data saved to {output_file}")
+    
     # Access and process the data as needed
     # For example, you can print the JSON data
-    print(data)
+    topics = data['topic_list']
+
+      # Specify the output file name
+    output_file = "Topics_list.json"
+    
+    # Write the JSON data to the output file
+    with open(output_file, 'w') as json_file:
+        json.dump(topics, json_file, indent=4)
+    
+    print(f"Data saved to {output_file}")
+    
+    slug = topics['topics']
+    actuallySlug = slug[0]['slug']
+    output_file = "slugTime"
+    with open(output_file, 'w') as json_file:
+        json.dump(slug, json_file, indent=4)
+    print(actuallySlug)
 else:
     print(f"Failed to retrieve data. Status code: {response.status_code}")
